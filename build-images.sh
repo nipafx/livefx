6d8e3ebbfd5c976aecc327c30f3364ac7e6bf1bf
Nicolai Parlog <nipa@codefx.org>
2021-03-07 18:42:46 +0100

    Fix errors in class path image creation

--- build-images.sh
+++ build-images.sh
@@ -13,8 +13,8 @@ jlink \
 mkdir target/linux-image/app
 cp target/calendar.jar target/linux-image/app
 cp src/image/application.properties target/linux-image/app
-cp src/image/calendar target/linux-image/bin
-tar -czf target/calendar-linux.tar.gz target/linux-image
+cp src/image/calendar target/linux-image
+tar -czf target/calendar-linux.tar.gz -C target/linux-image .
 echo "Done"
 
 echo ""
@@ -25,13 +25,13 @@ echo ""
 curl --silent https://download.java.net/java/GA/jdk16/7863447f0ab643c585b9bdebf67c69db/36/GPL/openjdk-16_windows-x64_bin.zip > target/windows-jdk.zip
 unzip -q target/windows-jdk.zip -d target/windows-jdk
 jlink \
-	--module-path target/windows-jdk/jmods \
+	--module-path target/windows-jdk/jdk-16/jmods \
 	--compress=2 --no-header-files --no-man-pages --strip-debug \
 	--output target/windows-image \
 	--add-modules java.desktop,java.naming
 mkdir target/windows-image/app
 cp target/calendar.jar target/windows-image/app
 cp src/image/application.properties target/windows-image/app
-cp src/image/calendar.bat target/windows-image/bin
-zip -qr target/calendar-windows.zip target/windows-image
+cp src/image/calendar.bat target/windows-image
+cd target/windows-image && zip -qr ../calendar-windows.zip * && cd -
 echo "Done"
--- src/image/calendar.bat
+++ src/image/calendar.bat
@@ -1,1 +1,3 @@
-rem TODO
+@echo off
+SET mypath=%~dp0
+%mypath%\bin\java --class-path %mypath%\app;%mypath%\app\calendar.jar org.springframework.boot.loader.PropertiesLauncher
