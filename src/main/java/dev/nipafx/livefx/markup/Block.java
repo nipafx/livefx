91cc1f6939ed8faaaf4d6a932b9c0166db87357f
Nicolai Parlog <nicolai@nipafx.dev>
2023-05-02 19:10:18 +0200

    Include block type in JSON to correctly display chat messages

--- src/main/java/dev/nipafx/livefx/markup/Block.java
+++ src/main/java/dev/nipafx/livefx/markup/Block.java
@@ -1,12 +1,21 @@
 package dev.nipafx.livefx.markup;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
 import java.util.Optional;
 
+import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME;
+
+@JsonTypeInfo(property = "type", use = NAME)
 public sealed interface Block {
 
 	String text();
 
+	@JsonTypeName("paragraph")
 	record Paragraph(String text) implements Block { }
+
+	@JsonTypeName("code")
 	record Code(String text, Optional<CodeBlockLanguage> language) implements Block { }
 
 }
